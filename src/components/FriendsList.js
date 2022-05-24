d9c1e3d4ef2800e97fb9c2e6cc1536efc9c0148c
unknown <macrowell@yahoo.com>
2022-05-24 19:21:49 -0400

    FriendsList component renders list of friends when token is present

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const initFriendsState = []
 
 const FriendsList = () => {
+    const [friendsState, setFriendsState] = useState(initFriendsState)
+
+    useEffect(() => {
+        axios.get('http://localhost:9000/api/friends', {
+        headers: {
+            authorization: localStorage.getItem("token")
+        }
+        })
+        .then(res => {
+            setFriendsState(...friendsState, res.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    console.log(friendsState)
+
     return (
         <div className="friends-list" >
             <h1>Friends List</h1>
+            {friendsState.length > 0 ? friendsState.map(friend => (
+                <div key={friend.id}>
+                    <p>{friend.name} - {friend.email}</p>
+                </div>
+            )) : <p>Loading...</p>}
         </div>
     )
 }
